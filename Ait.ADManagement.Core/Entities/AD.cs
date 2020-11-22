66138a38663869236947f15315e465bd40c6ee8d
Administrator@AITG1.LOCAL <Administrator@AITG1.LOCAL>
2020-11-22 13:24:58 +0100

    Add project files.

---
+++ Ait.ADManagement.Core/Entities/AD.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
+
+
+namespace Ait.ADManagement.Core.Entities
+{
+    public static class AD
+    {
+        public static string LDAPLong = "LDAP://127.0.0.1:389";
+        public static string LDAPShort = "LDAP://";
+        public static string ADDomainEmail = "@aitg1.local";
+        public static string ADDomainNameShort = "aitg1";
+        public static string ADDomainNameLong = "aitg1.local";
+
+
+    }
+}
